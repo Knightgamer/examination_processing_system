d5d564bf2376fa3c72dca95d483a260929382d78
iWB-Git <sbp1784@gmail.com>
2023-11-09 13:02:29 +0300

    maded some changes

--- server/server.js
+++ server/server.js
@@ -1,21 +1,26 @@
 const express = require("express");
-const errorHandler = require("./middleware/errorHandler2");
+const errorHandler = require("./middleware/errorHandler2.js");
 const connectDb = require("./config/dbConnection");
-// const connectDb = require("./config/dbConnection");
+const cors = require("cors"); // Add CORS middleware
+
 const app = express();
 const dotenv = require("dotenv").config();
-const port = process.env.PORT || 5001; // Use the environment port if available, or use port 3000 as a default
+const port = process.env.PORT || 5001;
 
 connectDb();
-// Define a route that responds with "Hello, World!" when you access the root URL
+
+app.use(express.json());
+
+// Enable CORS to allow requests from your React frontend
+app.use(cors());
+
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
-app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoutes"));
 app.use(errorHandler);
-// Start the server
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
