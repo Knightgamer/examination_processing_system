593e694259002d228d5339839098cb5cd4d10f38
iWB-Git <sbp1784@gmail.com>
2023-11-14 22:07:00 +0300

    vercel test 2

--- server/server.js
+++ server/server.js
@@ -1,24 +1,26 @@
 const express = require("express");
-// const errorHandler = require("./middleware/errorHandler2.js");
-// const connectDb = require("./config/dbConnection");
-// const cors = require("cors"); // Add CORS middleware
+const errorHandler = require("./middleware/errorHandler2.js");
+const connectDb = require("./config/dbConnection");
+const cors = require("cors"); // Add CORS middleware
 
 const app = express();
-// const dotenv = require("dotenv").config();
-// const port = process.env.PORT || 5001;
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 5001;
 
-// connectDb();
+connectDb();
 
-// app.use(express.json());
+app.use(express.json());
 
 // Enable CORS to allow requests from your React frontend
-// app.use(cors());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
-// app.use("/api/contacts", require("./routes/contactRoutes"));
-// app.use(errorHandler);
+app.use("/api/contacts", require("./routes/contactRoutes"));
+app.use(errorHandler);
 
-app.listen(5000, console.log(`Server is running on port 5000`));
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
