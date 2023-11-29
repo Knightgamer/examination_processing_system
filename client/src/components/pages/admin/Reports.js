6ec7a04c32ddcc284c97cf188585149fec1d1d49
iWB-Git <sbp1784@gmail.com>
2023-11-29 20:37:44 +0300

    added reports by semester

--- client/src/components/pages/admin/Reports.js
+++ client/src/components/pages/admin/Reports.js
@@ -1,7 +1,13 @@
 import React from "react";
+import SemesterReportsTable from "./Reports/SemesterReportsTable";
 
 function Reports() {
-  return <div>reports</div>;
+  return (
+    <div>
+      <h1 className="text-3xl font-semibold text-center mb-8">Reports</h1>
+      <SemesterReportsTable />
+    </div>
+  );
 }
 
 export default Reports;
