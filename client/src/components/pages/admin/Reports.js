0b8c697d39023fa5cfeb4ff079a26017b25bb2a1
iWB-Git <sbp1784@gmail.com>
2023-11-29 22:39:49 +0300

    Added a YearReportsTable component to the Reports page.

--- client/src/components/pages/admin/Reports.js
+++ client/src/components/pages/admin/Reports.js
@@ -1,11 +1,13 @@
 import React from "react";
 import SemesterReportsTable from "./Reports/SemesterReportsTable";
+import YearReportsTable from "./Reports/YearReportsTable";
 
 function Reports() {
   return (
     <div>
       <h1 className="text-3xl font-semibold text-center mb-8">Reports</h1>
       <SemesterReportsTable />
+      <YearReportsTable />
     </div>
   );
 }
