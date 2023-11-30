6a73d397ee59769437e9f9b2171c321635b16c8c
iWB-Git <sbp1784@gmail.com>
2023-11-30 12:47:16 +0300

    Added functionality to show whether a student has been graded or not.

--- client/src/components/pages/lecturer/LecturerCourses.js
+++ client/src/components/pages/lecturer/LecturerCourses.js
@@ -50,13 +50,48 @@ function LecturerCourses() {
   };
 
   const handleStudentSelect = (student) => {
-    // Construct the key for checking if student has been graded
     const gradedKey = `${student._id}_${selectedCourse._id}_${selectedCourse.semester}`;
-    if (!gradedStudents[gradedKey]) {
+    const isGraded = !!gradedStudents[gradedKey];
+
+    if (!isGraded) {
       setSelectedStudent(student);
       setShowScoreEntryForm(true);
     }
   };
+
+  useEffect(() => {
+    const fetchScores = async () => {
+      const newGradedStudents = {};
+
+      for (let student of students) {
+        try {
+          const response = await axios.get(
+            `http://localhost:5000/scores/${student._id}/${selectedCourse._id}`
+          );
+          const scoreData = response.data;
+
+          // Check if score data is available for the student
+          if (scoreData && scoreData._id) {
+            const gradedKey = `${student._id}_${selectedCourse._id}_${selectedCourse.semester}`;
+            newGradedStudents[gradedKey] = true;
+          }
+        } catch (error) {
+          console.error(
+            "Error fetching scores for student:",
+            student._id,
+            "; error:",
+            error.response ? error.response.data : error.message
+          );
+        }
+      }
+
+      setGradedStudents(newGradedStudents);
+    };
+
+    if (students.length > 0 && selectedCourse) {
+      fetchScores();
+    }
+  }, [students, selectedCourse]);
 
   const handleScoreFormClose = (gradedStudentId) => {
     if (gradedStudentId) {
@@ -100,19 +135,35 @@ function LecturerCourses() {
             <ul>
               {students.map((student) => {
                 const gradedKey = `${student._id}_${selectedCourse._id}_${selectedCourse.semester}`;
+                const isGraded = !!gradedStudents[gradedKey];
+
                 return (
-                  <li
+                  <div
                     key={student._id}
-                    className={`mb-1 cursor-pointer ${
-                      gradedStudents[gradedKey]
-                        ? "text-green-500"
-                        : "text-blue-500 hover:underline"
-                    }`}
-                    onClick={() => handleStudentSelect(student)}
+                    className="flex items-center justify-between bg-white shadow-sm p-4 mb-2 rounded-lg"
                   >
-                    {student.name} - {student.email}
-                    {gradedStudents[gradedKey] && <span> ✔️</span>}
-                  </li>
+                    <span
+                      className={`${
+                        isGraded ? "text-green-600" : "text-gray-800"
+                      } text-lg font-medium`}
+                    >
+                      {student.name}
+                      {isGraded && (
+                        <span className="ml-2 text-green-600">✔️</span>
+                      )}
+                    </span>
+                    <button
+                      onClick={() => handleStudentSelect(student)}
+                      disabled={isGraded}
+                      className={`px-4 py-2 text-sm font-medium rounded-md transition-colors ${
+                        isGraded
+                          ? "bg-gray-200 text-gray-500 cursor-not-allowed"
+                          : "bg-blue-500 text-white hover:bg-blue-600"
+                      }`}
+                    >
+                      {isGraded ? "Graded" : "Grade"}
+                    </button>
+                  </div>
                 );
               })}
             </ul>
